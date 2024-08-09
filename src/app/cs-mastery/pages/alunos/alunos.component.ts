0d0af89e756dcfde3e600e78e6409f354f9fc693
Herbeth Milhome <h2milhome@gmail.com>
2024-08-09 14:25:30 -0300

    improvement import of aluno

--- src/app/cs-mastery/pages/alunos/alunos.component.ts
+++ src/app/cs-mastery/pages/alunos/alunos.component.ts
@@ -4,6 +4,9 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { AppMaterialModule } from '../../../shared/app-material/app-material.module';
 import { AlunosListComponent } from './alunos-list/alunos-list.component';
 import { AlunoImportComponent } from "./aluno-import/aluno-import.component";
+import { MatDialog } from '@angular/material/dialog';
+import { ImportAlunoDialogComponent } from './aluno-import/import-aluno-dialog/import-aluno-dialog.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -17,11 +20,30 @@ export class AlunosComponent {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private snackBar: MatSnackBar
   ){ }
 
   newAluno() {
     this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
+  onImport() {
+    const dialogRef = this.dialog.open(ImportAlunoDialogComponent, {
+    });
+
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+
+      if(result) {
+        this.openSnackBar('Alunos importados com sucesso!', 'Fechar');
+      }
+
+    });
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, { duration: 5000, verticalPosition: 'top', horizontalPosition: 'center' });
   }
 
 }
